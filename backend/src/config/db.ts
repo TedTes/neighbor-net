f5f7b667a8084af9cb37e6bca4bf6798c57e66f5
Tedros <tedtfu@gmail.com>
2024-09-16 11:28:18 -0400

    docker setup cont.., db conn fixs

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,5 +3,5 @@ export const db = {
   mongoUri:
     process.env.MONGODB_URI ||
     process.env.MONGO_HOST ||
-    "mongodb://root:123@mongodb:27017/neighbor-net?directConnection=true&serverSelectionTimeoutMS=10000",
+    `mongodb://root:123@mongodb:27017/neighbor-net?authSource=admin&directConnection=true&serverSelectionTimeoutMS=10000`,
 };
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,16 +3,20 @@ import { db } from "./config";
 import { logger } from "./utils/logger";
 import app from "./app";
 
-app.listen(db.port, () => {
+app.listen(db.port, async () => {
   logger.info(`server running on port:${db.port}`);
-});
-
-mongoose.Promise = global.Promise;
-mongoose.connect(db.mongoUri);
-mongoose.connection.on("error", () => {
-  throw new Error(`unable to connect to database: ${db.mongoUri}`);
-});
-
-mongoose.connection.on("connected", () => {
-  logger.info(`connected to mongodb`);
+  try {
+    mongoose.Promise = global.Promise;
+    await mongoose
+      .connect(db.mongoUri)
+      .then(() => {
+        logger.debug(`connected to mongodb`);
+      })
+      .catch((error) => {
+        logger.debug(JSON.stringify(error));
+      });
+  } catch (error) {
+    logger.debug("from catch");
+    logger.debug(JSON.stringify(error));
+  }
 });
